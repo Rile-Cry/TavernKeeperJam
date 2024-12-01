e07b1fcafaf208884da6206add340df0e5ccbf40
Rile-Cry <52363024+Rile-Cry@users.noreply.github.com>
2024-12-01 14:02:58 -0500

    Project Setup
    Added in Dialogic 2 and cleaned out the 3D components that won't be needed.

--- autoload/persistence/settings/input_controls.gd
+++ autoload/persistence/settings/input_controls.gd
@@ -5,27 +5,8 @@ const MoveLeft: StringName = &"move_left"
 const MoveForward: StringName = &"move_forward"
 const MoveBack: StringName = &"move_back"
 
-const CrouchAction: StringName = &"crouch"
-const CrawlAction: StringName = &"crawl"
-const RunAction: StringName = &"run"
-const JumpAction: StringName = &"jump"
-
 const Interact: StringName = &"interact"
 const CancelInteraction: StringName = &"cancel_interact"
-
-const Pull: StringName = &"pull"
-const PullArea: StringName = &"pull_area"
-const Drop: StringName = &"drop"
-const Throw: StringName = &"throw"
-const PushWave: StringName = &"push_wave"
-
-const Aim: StringName = &"aim"
-const Shoot: StringName = &"shoot"
-
-const PrimaryWeapon: StringName = &"primary_weapon"
-const SecondaryWeapon: StringName = &"secondary_weapon"
-const HeavyWeapon: StringName = &"heavy_weapon"
-const MeleeWeapon: StringName = &"melee_weapon"
 
 const Drag: StringName = &"drag"
 

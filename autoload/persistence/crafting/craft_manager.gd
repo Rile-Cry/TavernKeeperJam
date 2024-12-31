c3060aad0732c1c311358ca1fff77d7922bd539c
Matas <matas.kaz08@gmail.com>
2024-12-31 16:33:36 +0200

    export working

--- autoload/persistence/crafting/craft_manager.gd
+++ autoload/persistence/crafting/craft_manager.gd
@@ -6,6 +6,74 @@ var recipes := {}
 var _ingredient_path := "res://components/crafting/items"
 var _recipe_path := "res://components/crafting/recipes"
 
+var ingredients_array = [
+	preload("res://components/crafting/items/ingredients/ale.tres"),
+	preload("res://components/crafting/items/ingredients/beer.tres"),
+	preload("res://components/crafting/items/ingredients/chopped_firepepper.tres"),
+	preload("res://components/crafting/items/ingredients/chopped_fortiflower.tres"),
+	preload("res://components/crafting/items/ingredients/chopped_galegrass.tres"),
+	preload("res://components/crafting/items/ingredients/chopped_galegrass_a.tres"),
+	preload("res://components/crafting/items/ingredients/chopped_hartroot.tres"),
+	preload("res://components/crafting/items/ingredients/cider.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_firepepper.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_firepepper_a.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_fortiflower.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_galegrass.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_galegrass_a.tres"),
+	preload("res://components/crafting/items/ingredients/crushed_hartroot.tres"),
+	preload("res://components/crafting/items/ingredients/empty_drink.tres"),
+	preload("res://components/crafting/items/ingredients/gin.tres"),
+	preload("res://components/crafting/items/ingredients/glass.tres"),
+	preload("res://components/crafting/items/ingredients/glass_with_firepepper_gin.tres"),
+	preload("res://components/crafting/items/ingredients/glass_with_strength_gin.tres"),
+	preload("res://components/crafting/items/ingredients/glass_with_swiftness_gin.tres"),
+	preload("res://components/crafting/items/ingredients/hartroot.tres"),
+	preload("res://components/crafting/items/ingredients/ruined_drink.tres"),
+	preload("res://components/crafting/items/ingredients/tankard.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_firepepper_ale.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_firepepper_beer.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_firepepper_cider.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_strength_ale.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_strength_beer.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_strength_cider.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_swiftness_ale.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_swiftness_beer.tres"),
+	preload("res://components/crafting/items/ingredients/tankard_with_swiftness_cider.tres"),
+	preload("res://components/crafting/items/ingredients/wine.tres"),
+	preload("res://components/crafting/items/ingredients/wineglass.tres"),
+	preload("res://components/crafting/items/ingredients/wineglass_with_firepepper_wine.tres"),
+	preload("res://components/crafting/items/ingredients/wineglass_with_strength_wine.tres"),
+	preload("res://components/crafting/items/ingredients/wineglass_with_swiftness_wine.tres"),
+]
+
+
+
+
+var recipes_array = [ 
+	preload("res://components/crafting/recipes/ale.tres"),
+	preload("res://components/crafting/recipes/beer.tres"), 
+	preload("res://components/crafting/recipes/chopped_hartroot.tres"), 
+	preload("res://components/crafting/recipes/cider.tres"), 
+	preload("res://components/crafting/recipes/empty_drink.tres"), 
+	preload("res://components/crafting/recipes/gin.tres"), 
+	preload("res://components/crafting/recipes/glass_with_firepepper_gin.tres"), 
+	preload("res://components/crafting/recipes/glass_with_strength_gin.tres"), 
+	preload("res://components/crafting/recipes/glass_with_swiftness_gin.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_firepepper_ale.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_firepepper_beer.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_firepepper_cider.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_strength_ale.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_strength_beer.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_strength_beer_a.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_strength_cider.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_swiftness_ale.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_swiftness_beer.tres"), 
+	preload("res://components/crafting/recipes/tankard_with_swiftness_cider.tres"), 
+	preload("res://components/crafting/recipes/wine.tres"), 
+	preload("res://components/crafting/recipes/wineglass_with_firepepper_wine.tres"), 
+	preload("res://components/crafting/recipes/wineglass_with_strength_wine.tres"), 
+	preload("res://components/crafting/recipes/wineglass_with_swiftness_wine.tres"),
+]
 
 func craft(ingredient_list: Array[String], process: TavernItem.PROCESS_TYPE) -> TavernItem:
 	var craft_list : Array[TavernItem] = []
@@ -27,35 +95,35 @@ func _recipe_check(ingredient_list: Array[TavernItem], process: TavernItem.PROCE
 	return null
 
 func _load_ingredients() -> void:
-	var temp_ingredients := _load_dir(_ingredient_path)
+	var temp_ingredients = ingredients_array
 	for ingredient in temp_ingredients:
-		var ingredient_resource : TavernItem = ResourceLoader.load(_ingredient_path + "/" + ingredient, "TavernItem")
+		var ingredient_resource : TavernItem = ingredient
 		var ingredient_name : String = ingredient_resource.ingredient_name
 		ingredients[ingredient_name] = ingredient_resource
 	
 
 func _load_recipes() -> void:
-	var temp_recipes := _load_dir(_recipe_path)
+	var temp_recipes = recipes_array
 	for recipe in temp_recipes:
-		var recipe_resource : TavernRecipe = ResourceLoader.load(_recipe_path + "/" + recipe, "TavernRecipe")
+		var recipe_resource : TavernRecipe = recipe
 		var recipe_name : String = recipe_resource.result.ingredient_name
 		recipes[recipe_name] = recipe_resource
 
-func _load_dir(path: String, sub_dir: String = "") -> Array[String]:
-	var dir = DirAccess.open(path + sub_dir)
-	var list : Array[String] = []
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				list += _load_dir(path, sub_dir + "/" + file_name)
-			else:
-				list.append(sub_dir + "/" + file_name)
-			file_name = dir.get_next()
-		return list
-	else:
-		return []
+#func _load_dir(path: String, sub_dir: String = "") -> Array[String]:
+#	var dir = DirAccess.open(path + sub_dir)
+#	var list : Array[String] = []
+#	if dir:
+#		dir.list_dir_begin()
+#		var file_name = dir.get_next()
+#		while file_name != "":
+#			if dir.current_is_dir():
+#				list += _load_dir(path, sub_dir + "/" + file_name)
+#			else:
+#				list.append(sub_dir + "/" + file_name)
+#			file_name = dir.get_next()
+#		return list
+#	else:
+#		return []
 
 func has_all(array1 : Array[TavernItem], array2: Array[TavernItem] ) ->bool:
 	var array_contains : bool = false
